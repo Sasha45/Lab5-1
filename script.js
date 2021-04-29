4ee8143553cc6a0bf8fe37cacf97aecebcc274d8
Sasha <okubichk@ucsd.edu>
2021-04-29 14:59:52 -0700

    Finally fix listener call

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 // script.js
 
-const img = new Image(); // used to load image from <input> and draw to canvas
+var ctr = 0;
 
-// Fires whenever the img object loads a new image (such as with img.src =)
-img.addEventListener('load', () => {
-  var image = document.getElementById('input').files[0]
+//placeholder for uploaded image
+var img = new Image()
+img.src = "images/lab.jpg";
+
+//the image upload button
+const img_input = document.getElementById("image-input");
+//add listener to image uplaod
+img_input.addEventListener('input', onUploaderBrush());
+
+function onUploaderBrush(){
+  alert("AAAAAAAAAAAAAA");
+}
+
+
+const submit_button = document.querySelector("[type = 'submit']");
+submit_button.textContent = "hello";
+
+submit_button.addEventListener('click', doStuffPleaseGod);
+
+//whenever *anything* happens, apparently. sheesh.
+function doStuffPleaseGod(){
+  ctr += 1;
+  submit_button.textContent = "aaaaaa" + ctr;
+
+  //just to make sure this is running
+  //alert("Hello there!");
+
+
+  var file = img_input.files[0];
+  if(img != null){
+    //img.src = URL.createObjectURL(file);
+    img.src = "images/lab.jpg"
+  }
+  
+
+
   // TODO
-  console.log("listener fired!")
+  console.log("listener fired!");
   const canvas = document.getElementById('user-image');
   const ctx = canvas.getContext('2d');
   // Some helpful tips:
@@ -18,9 +51,16 @@ img.addEventListener('load', () => {
   // - Clear the form when a new image is selected
 
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
-  let gotDimensions = getDimensions(canvas.width, canvas.height, image.width, image.height);
+  let gotDimensions = getDimensions(canvas.width, canvas.height, img.width, img.height);
+  ctx.drawImage(img, gotDimensions.startX, gotDimensions.startY, gotDimensions.width, gotDimensions.height);
+  document.body.appendChild(img);
+}
 
-});
+
+//stops the submit button reloading the page
+document.querySelector("[type='submit']").addEventListener("click", function(event) {event.preventDefault()});
+
+
 
 /**
  * Takes in the dimensions of the canvas and the new image, then calculates the new
